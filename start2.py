a8762927f77e36079c068d9961e6e935d3ca4e28
TaherAbbasi <abbasi.taher@GMail.com>
2020-12-21 16:40:06 -0700

    config.ini sample

--- start2.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='A package for handling sensys speed camera')
-
-parser.add_argument('--SetconfigFile', action="store", dest="config_file")
-
-args = parser.parse_args()
-print(args)
